98584638aa349e41ed5434daf45514143a0294fa
david123456858 <juandavidperaltafuentes@gmail.com>
2026-08-25 21:25:36 -0500

    feat: Actualiza la configuración de conexión a la base de datos y mejora el .gitignore

--- casos/01-transferencia-concurrente/index.ts
+++ casos/01-transferencia-concurrente/index.ts
@@ -1,1 +1,33 @@
 // TODO: lo escribo yo
+import { Client } from "pg"
+
+
+const config = {
+    user: "lab",
+    password: "lab",
+    host: "localhost",
+    port: 5432,
+    database: "backend_lab",
+    query_timeout: 1000,
+    connectionTimeoutMillis: 80000
+}
+
+export const client = new Client(config)
+
+await client.connect()
+    .then(() => {
+        console.log("Connection correctly");
+    })
+    .catch((reason) => {
+        console.log(reason);
+        
+    })
+
+await client.end()
+    .then(() => {
+        console.log("bay bay database closed");
+
+    }).catch(() => {
+        console.log("Hubo un error al cerra la conexion");
+        
+    })
