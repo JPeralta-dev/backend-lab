553e9e9e876ae5aa69d58a759e890735cddcae87
david123456858 <juandavidperaltafuentes@gmail.com>
2026-09-24 20:50:24 -0500

    feat: Implementa simulación de 150 peticiones simultáneas y mide tiempos de ejecución en el Connection Pool

--- casos/03-connection-pool/index.ts
+++ casos/03-connection-pool/index.ts
@@ -17,15 +17,51 @@ async function main() {
   console.log("🚀 Iniciando prueba de estrés sobre el Connection Pool...");
   console.log(`⚙️ Configuración del pool: max = ${poolConfig.max}, timeout = ${poolConfig.connectionTimeoutMillis}ms`);
 
-  const TOTAL_REQUESTS = 150;
-
-  // TODO: Escribe aquí la simulación de 150 peticiones simultáneas:
-  // 1. Lanza 150 tareas concurrentes (puedes usar Promise.allSettled).
+  // 1. Lanza 150 tareas concurrentes (puedes usar Promise.allSettled). ✅
   // 2. Cada tarea debe pedir una conexión al pool (o hacer un query simulando latencia, ej. "SELECT pg_sleep(0.1)").
   // 3. Mide cuántas peticiones terminan con éxito y cuántas fallan por timeout.
   // 4. Mide los tiempos de espera y latencia.
   // 5. Imprime un resumen de métricas (éxitos, fallos, códigos de error).
 
+  const TOTAL_REQUESTS = 150;
+  const REQUEST = `SELECT pg_sleep(0.1) * FROM transactions WHERE created_at BETWEEN '2025-01-14' AND '2025-06-14'`
+
+  const executeMetrics = async (id:number) => {
+    const start = performance.now()
+    try {
+      const result = await pool.query(REQUEST)
+      const end = performance.now()
+      return {
+        id,
+        result,
+        success: true,
+        time: end - start
+      }
+    } catch (error) {
+      const end = performance.now();
+      return {
+        id,
+        error,
+        success: true,
+        time: end - start
+      }
+    }
+  }
+  const taks = []
+  // TODO: Escribe aquí la simulación de 150 peticiones simultáneas:
+  
+  for (let index = 0; index < TOTAL_REQUESTS; index++) {
+    taks.push(executeMetrics(index))
+  }
+
+  const resultado = await Promise.allSettled(taks)
+
+
+
+  console.log(`Las request que fueron ejecutadas correctamente fueron: ${resultado.filter((i) => { i.status == "fulfilled" }).length}`);
+  console.log(`Las request que fueron ejecutadas con error fueron: ${resultado.filter((i)=>{ i.status == "rejected"}).length}`);
+  
+  
   await pool.end();
 }
 
